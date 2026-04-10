4e52684576696bade097e2cd4629782c42d0f72b
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-10 21:54:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById("mainScreen").style.display = "none";
+    document.getElementById("loginForm").style.display = "flex";
+}
+
+function showRegister() {
+    document.getElementById("mainScreen").style.display = "none";
+    document.getElementById("registerForm").style.display = "flex";
+}
