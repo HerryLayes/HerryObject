84cbfab838f61b53ca9c9b260049877106a93965
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-10 22:19:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function showLogin() {
-    document.getElementById("mainScreen").style.display = "none";
-    document.getElementById("loginForm").style.display = "flex";
-}
+function register() {
+    let username = document.getElementById("regUsername").value;
+    let password = document.getElementById("regPassword").value;
 
-function showRegister() {
-    document.getElementById("mainScreen").style.display = "none";
-    document.getElementById("registerForm").style.display = "flex";
+    if (username === "" || password === "") {
+        alert("Заполни все поля!");
+        return;
+    }
+
+    // Сохраняем данные
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    // Переход на страницу home
+    window.location.href = "home.html";
 }
