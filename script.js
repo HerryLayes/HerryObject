ce5866233acb59270e506baaacf08c4cd8495dec
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-26 21:50:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// ===== ничего =====
+const modal = document.getElementById("modal");
+const openBtn = document.querySelector(".main-btn");
+const closeBtn = document.getElementById("closeBtn");
 
+// открыть
+openBtn.onclick = () => {
+  modal.classList.remove("hidden");
+};
+
+// закрыть
+closeBtn.onclick = () => {
+  modal.classList.add("hidden");
+};
+
+// закрытие по клику вне окна
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+  }
+};
