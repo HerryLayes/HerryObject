7ab5f53fe28f05e7930e266a1d387ec324f01f7f
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-26 21:39:18 +0300

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-let username = localStorage.getItem(username);
-
-if (username) {
-    document.getElementById(userName).textContent = username;
-} else {
-     если нет аккаунта — назад
-    window.location.href = index.html;
-}
