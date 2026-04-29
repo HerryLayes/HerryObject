cbed2bf4f69f19aec23e433d04ef8d0d6b4a1887
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-29 18:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -623,11 +623,10 @@ function move() {
   animate();
 
   // РЕСАЙЗ
-  window.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  });
-}
+ window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
   window.openEditor = openEditor;
 });
