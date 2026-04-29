d3473d5913672b719103ec697a1defd2100a0746
HerryLayes <gigion9000Roblox@yandex.ru>
2026-04-29 17:35:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -488,7 +488,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  1000)
 
 document.body.innerHTML = `
   <div id="editorUI" style="
